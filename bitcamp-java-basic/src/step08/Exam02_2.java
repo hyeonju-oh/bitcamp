74de39c6b5a4dc6293f34f4b6b3bc549b70e2122
hyeonju-oh <hyeonju3212@gmail.com>
2018-03-26 08:54:13 +0900

    '.'

--- bitcamp-java-basic/src/step08/Exam02_2.java
+++ bitcamp-java-basic/src/step08/Exam02_2.java
@@ -15,7 +15,7 @@ public class Exam02_2 {
     }
     
     public static void main(String[] args) {
-        
+          
         Scanner keyScan = new Scanner(System.in);
         System.out.print("숫자를 입력하세요> ");
         int value = keyScan.nextInt();
--- bitcamp-java-basic/src/step08/Exam05_3.java
+++ bitcamp-java-basic/src/step08/Exam05_3.java
@@ -1,19 +1,17 @@
-package step08;
 // 변수 초기화 - 초기화 문장과 생성자의 실행 순서
+package step08;
+
 public class Exam05_3 {
     static class A {
-        int a = 100;
-        // 생성자는 초기화 문장을 실행한 후에 호출된다
+        int a = 200;
+        
+        // 생성자는 초기화 문장을 실행한 후에 호출된다.
         A() {
             this.a = 300;
         }
     }
     public static void main(String[] args) {
-        
         A obj1 = new A();
         System.out.println(obj1.a);
-        
-    }
+    } 
 }
-
-// 생성자? 인스턴스(객체)를 생성한 후에 사용하기 전에 유효한 값으로 초기화시키는 작업을 수행한다
