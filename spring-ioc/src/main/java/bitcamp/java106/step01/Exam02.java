62aa996c91839aaf9e87e2624aed6ac0d347a3f1
ohhyeonju <hyeonju3212@gmail.com>
2018-05-04 09:12:58 +0900

    .

--- spring-ioc/src/main/java/bitcamp/java106/step01/Exam02.java
+++ spring-ioc/src/main/java/bitcamp/java106/step01/Exam02.java
@@ -25,7 +25,7 @@ public class Exam02 {
         //    또 소스 코드를 변경해야 하는 문제가 있다. 
         //    그래서 보통 ClassPathXmlApplicationContext를 많이 사용한다.
         iocContainer = new FileSystemXmlApplicationContext(
-                "file:///Users/eomjinyoung/git/java106/java106-spring-ioc/src/main/java/bitcamp/java106/step01/application-context.xml");
+                "file:\\C:\\Users\\Bit\\git\\bitcamp\\spring-ioc\\src\\main\\java\\bitcamp\\java106\\step01\\application-context.xml");
         
         // IoC 컨테이너에 보관된 객체를 꺼내기
         Car car = (Car) iocContainer.getBean("c1");
