c782c6f2162b161a85156d499d1c2c89e84e9f48
ohhyeonju <hyeonju3212@gmail.com>
2018-05-23 17:09:50 +0900

    .

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/listener/ContextLoaderListener.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/listener/ContextLoaderListener.java
@@ -1,5 +1,6 @@
 package bitcamp.java106.pms.listener;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -7,7 +8,6 @@ import javax.servlet.annotation.WebListener;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import bitcamp.java106.pms.AppConfig;
 import bitcamp.java106.pms.support.WebApplicationContextUtils;
 
 @WebListener
@@ -15,9 +15,13 @@ public class ContextLoaderListener implements ServletContextListener {
     
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        ServletContext sc = sce.getServletContext();
+        String configClassName = sc.getInitParameter("contextConfigLocation");
+        
+        try {
         // 서블릿 컨테이너가 시작되면 스프링 IoC 컨테이너를 준비한다.
         ApplicationContext iocContainer = 
-                new AnnotationConfigApplicationContext(AppConfig.class);
+                new AnnotationConfigApplicationContext(Class.forName(configClassName));
         
         // 서블릿에서 스프링 IoC 컨테이너를 꺼내 쓸 수 있도록,
         // WebApplicationContextUtils에 보관한다.
@@ -27,6 +31,9 @@ public class ContextLoaderListener implements ServletContextListener {
         // 왜, 이렇게 복잡하게 하는가?
         // => 스프링을 모방하기 위함이다.
         // => 그냥 그렇게 이해하라!
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
