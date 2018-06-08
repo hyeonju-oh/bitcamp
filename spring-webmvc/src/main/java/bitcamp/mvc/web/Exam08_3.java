a2a01bcce9548cecfc91459896d08cd0f6d62917
ohhyeonju <hyeonju3212@gmail.com>
2018-06-08 12:51:02 +0900

    .

--- spring-webmvc/src/main/java/bitcamp/mvc/web/Exam08_3.java
+++ spring-webmvc/src/main/java/bitcamp/mvc/web/Exam08_3.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @RequestMapping("/exam08_3")
 // Model(또는 Map) 객체에 저장하는 데이터 중에서 HttpSession 객체에도 저장해야 한다면,
 // 다음 애노테이션으로 해당 값의 이름을 지정하라!
-@SessionAttributes({"name", "age"})
+@SessionAttributes({"name", "age", "working"})
 // 의미?
 // => name, age 라는 이름으로 Model(또는 Map) 객체에 저장하는 값들을
 //    ServletRequest 뿐만아니라 HttpSession에도 저장하라는 뜻이다!
@@ -26,7 +26,7 @@ public class Exam08_3 {
         // => Model이나 Map 객체에 데이터를 저장하면, JSP에서 꺼내 쓸 수 있도록
         //    프론트 컨트롤러가 ServletRequest 보관소로 복사한다.
         model.addAttribute("name", name);
-        return String.format("m1(): name=%s", name);
+        return String.format("m1(): 모델 객체를 통해서 세션에 값 넣기 - name=%s", name);
     }
     
     @GetMapping(value="m2", produces="text/plain;charset=UTF-8")
@@ -36,7 +36,7 @@ public class Exam08_3 {
         //      http://localhost:8888/spring-webmvc/mvc/exam08_3/m2?age=20
         // 세션에 데이터 보관
         model.addAttribute("age", age);
-        return String.format("m2(): age=%d", age);
+        return String.format("m2(): 모델 객체를 통해서 세션에 값 넣기 - age=%d", age);
     }
     
     @GetMapping(value="m3", produces="text/plain;charset=UTF-8")
@@ -46,6 +46,6 @@ public class Exam08_3 {
         //      http://localhost:8888/spring-webmvc/mvc/exam08_3/m3?working=true
         // 세션에 데이터 보관
         model.addAttribute("working", working);
-        return String.format("m3(): working=%b", working);
+        return String.format("m3(): 모델 객체를 통해서 세션에 값 넣기 - working=%b", working);
     }
 }
