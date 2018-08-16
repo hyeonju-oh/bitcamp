5a04ca1cbddfc640165bbc2c23b6a3ccc3cdcd36
ohhyeonju <hyeonju3212@gmail.com>
2018-08-16 15:58:31 +0900

    .

--- bitcamp-java-project/src/main/webapp/html7/js/common.js
+++ bitcamp-java-project/src/main/webapp/html7/js/common.js
@@ -1,1 +1,2 @@
-var serverRoot = "http://localhost:8888/bitcamp-java-project";
+//var serverRoot = "http://localhost:8888/bitcamp-java-project";
+var serverRoot = "http://13.125.234.107:8888/bitcamp-java-project";
